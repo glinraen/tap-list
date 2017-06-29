a28c0dab7e4fec3483de8147f637f5b4f837b415
GLINRAEN <MISTRESS.WYN@GMAIL.COM>
2017-06-29 12:24:11 -0700

    adds beer model

--- server.js
+++ server.js
@@ -1,5 +1,9 @@
 // Get the packages we need
 var express = require('express');
+var mongoose = require('mongoose');
+
+// Connect to the tap-list MongoDB
+mongoose.connect('mongodb://localhost:27017/tap-list');
 
 // Create our Express application
 var app = express();
