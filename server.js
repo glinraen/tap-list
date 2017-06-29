422b9d1cf22214f84c96aa3fd2bcb0a9b6337a87
GLINRAEN <MISTRESS.WYN@GMAIL.COM>
2017-06-29 13:00:12 -0700

    adds post, get, all

--- server.js
+++ server.js
@@ -27,6 +27,54 @@ router.get('/', function(req, res) {
   res.json({ message: 'You are running dangerously low on beer!' });
 });
 
+// Create a new route with the prefix /beers
+var beersRoute = router.route('/beers');
+
+// Create endpoint /api/beers for POSTS
+beersRoute.post(function(req, res) {
+  // Create a new instance of the Beer model
+  var beer = new Beer();
+
+// Create endpoint /api/beers for GET
+beersRoute.get(function(req, res) {
+  // Use the Beer model to find all beer
+  Beer.find(function(err, beers) {
+    if (err)
+      res.send(err);
+
+    res.json(beers);
+  });
+});
+
+// Create a new route with the /beers/:beer_id prefix
+var beerRoute = router.route('/beers/:beer_id');
+
+// Create endpoint /api/beers/:beer_id for GET
+beerRoute.get(function(req, res) {
+  // Use the Beer model to find a specific beer
+  Beer.findById(req.params.beer_id, function(err, beer) {
+    if (err)
+      res.send(err);
+
+    res.json(beer);
+  });
+});
+
+  // Set the beer properties that came from the POST data
+  beer.name = req.body.name;
+  beer.type = req.body.type;
+  beer.quantity = req.body.quantity;
+
+  // Save the beer and check for errors
+  beer.save(function(err) {
+    if (err)
+      res.send(err);
+
+    res.json({ message: 'Beer is added to the list!', data: beer });
+  });
+});
+
+
 // Register all our routes with /api
 app.use('/api', router);
 
